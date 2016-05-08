eb329225ff6cf8932459c141a761cb219813aeb6
Evin1- <edwin.hdz.12.0@gmail.com>
2016-05-08 17:38:04 -0400

    Add SQLCipher's SQLiteOpenHelper implementation

---
+++ app/src/main/java/com/example/simplesqlcipher/UsersDatabaseHelper.java
@@ -0,0 +1,42 @@
+package com.example.simplesqlcipher;
+
+import android.content.Context;
+
+import net.sqlcipher.database.SQLiteDatabase;
+import net.sqlcipher.database.SQLiteOpenHelper;
+
+/**
+ * Created by evin on 5/8/16.
+ */
+public class UsersDatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "usersDB";
+    private static final int DATABASE_VERSION = 1;
+
+    public static final String TABLE_USERS = "users";
+
+    public static final String KEY_USER_ID = "id";
+    public static final String KEY_USER_NAME = "userName";
+    public static final String KEY_AGE = "age";
+
+    public UsersDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String CREATE_USERS_TABLE = "CREATE TABLE " + TABLE_USERS +
+                "(" +
+                KEY_USER_ID + " INTEGER PRIMARY KEY," +
+                KEY_USER_NAME + " TEXT," +
+                KEY_AGE + " TEXT" +
+                ")";
+
+        db.execSQL(CREATE_USERS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERS);
+        onCreate(db);
+    }
+}
